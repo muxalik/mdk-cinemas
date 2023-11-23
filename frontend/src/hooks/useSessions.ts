e7453388ebb430981510ab7db9661b5537c55516
muxalik <muxail1327@mail.ru>
2023-11-23 12:48:40 +0300

    feature use sessions hook

---
+++ frontend/src/hooks/useSessions.ts
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react'
+import { pagination, session } from '../types'
+import api, { baseURL } from '../utils/api'
+
+const useSessions = (appliedFilters: any) => {
+  const [sortBy, setSortBy] = useState<string | null>(null)
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc')
+  const [query, setQuery] = useState('')
+  const [sessions, setSessions] = useState<session[] | null>(null)
+  const [pagination, setPagination] = useState<pagination>({
+    current: 1,
+    total: 0,
+    from: 0,
+    to: 0,
+    perPage: 10,
+  })
+
+  useEffect(() => {
+    api
+      .get(baseURL + '/sessions', {
+        params: {
+          page: pagination.current,
+          search: query,
+          sort: sortBy,
+          order: sortOrder,
+          min_ticket_price: appliedFilters.minTicketPrice,
+          max_ticket_price: appliedFilters.maxTicketPrice,
+          min_free_places: appliedFilters.minFreePlaces,
+          max_free_places: appliedFilters.maxFreePlaces,
+        },
+      })
+      .then((response) => {
+        setSessions(response.data.data)
+        setPagination({
+          current:
+            response.data.current_page || response.data.meta.current_page,
+          total: response.data.total || response.data.meta.total,
+          from: response.data.from || response.data.meta.from,
+          to: response.data.to || response.data.meta.to,
+          perPage: response.data.per_page || response.data.meta.per_page,
+        })
+      })
+      .catch(console.log)
+  }, [pagination.current, query, sortBy, sortOrder, appliedFilters])
+
+  const onPageChange = (page: number) =>
+    setPagination((prev) => {
+      return {
+        ...prev,
+        current: page,
+      }
+    })
+
+  const rows = sessions?.map((session) => {
+    return [
+      session.cinema,
+      session.movie,
+      session.ticket_price,
+      session.free_places.toString(),
+      session.starts_at,
+    ]
+  })
+
+  return {
+    sortBy,
+    setSortBy,
+    sortOrder,
+    setSortOrder,
+    query,
+    setQuery,
+    pagination,
+    setPagination,
+    onPageChange,
+    rows,
+  }
+}
+
+export default useSessions
--- frontend/src/pages/Sessions/index.tsx
+++ frontend/src/pages/Sessions/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import './styles.scss'
 import Layout from '../../layouts/Layout'
 import Breadcrumbs from '../../components/UI/Breadcrumbs'
@@ -6,8 +5,7 @@ import Button from '../../components/UI/Button'
 import { Variants } from '../../enums'
 import { cross, pdf, plus, search, slider, xlsx } from '../../assets'
 import TextField from '../../components/UI/TextField'
-import { pagination, session, sessionDefaultFilters } from '../../types'
-import api, { baseURL } from '../../utils/api'
+import { sessionDefaultFilters } from '../../types'
 import downloadFromUrl from '../../utils/downloadFromUrl'
 import Exports from '../../components/Exports'
 import Table from '../../components/UI/Table'
@@ -16,20 +14,9 @@ import Filters from '../../components/Filters'
 import Link from '../../components/UI/Link'
 import FilterGroup from '../../components/UI/FilterGroup'
 import useFilters from '../../hooks/useFilters'
+import useSessions from '../../hooks/useSessions'
 
 const Sessions = () => {
-  const [sortBy, setSortBy] = useState<string | null>(null)
-  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc')
-  const [query, setQuery] = useState('')
-  const [sessions, setSessions] = useState<session[] | null>(null)
-  const [pagination, setPagination] = useState<pagination>({
-    current: 1,
-    total: 0,
-    from: 0,
-    to: 0,
-    perPage: 10,
-  })
-
   const {
     appliedFilters,
     currentFilters,
@@ -43,51 +30,18 @@ const Sessions = () => {
     newFiltersAdded,
   } = useFilters(sessionDefaultFilters)
 
-  useEffect(() => {
-    api
-      .get(baseURL + '/sessions', {
-        params: {
-          page: pagination.current,
-          search: query,
-          sort: sortBy,
-          order: sortOrder,
-          min_ticket_price: appliedFilters.minTicketPrice,
-          max_ticket_price: appliedFilters.maxTicketPrice,
-          min_free_places: appliedFilters.minFreePlaces,
-          max_free_places: appliedFilters.maxFreePlaces,
-        },
-      })
-      .then((response) => {
-        setSessions(response.data.data)
-        setPagination({
-          current:
-            response.data.current_page || response.data.meta.current_page,
-          total: response.data.total || response.data.meta.total,
-          from: response.data.from || response.data.meta.from,
-          to: response.data.to || response.data.meta.to,
-          perPage: response.data.per_page || response.data.meta.per_page,
-        })
-      })
-      .catch(console.log)
-  }, [pagination.current, query, sortBy, sortOrder, appliedFilters])
-
-  const onPageChange = (page: number) =>
-    setPagination((prev) => {
-      return {
-        ...prev,
-        current: page,
-      }
-    })
-
-  const rows = sessions?.map((session) => {
-    return [
-      session.cinema,
-      session.movie,
-      session.ticket_price,
-      session.free_places.toString(),
-      session.starts_at,
-    ]
-  })
+  const {
+    sortBy,
+    setSortBy,
+    sortOrder,
+    setSortOrder,
+    query,
+    setQuery,
+    pagination,
+    setPagination,
+    onPageChange,
+    rows,
+  } = useSessions(appliedFilters)
 
   return (
     <Layout>
